e4257a55e6e452869aa5cb3f3dd9ddaaa0bf78b0
sbfm <fumi_siba@yahoo.co.jp>
2021-11-02 05:11:37 +0900

    pip対応

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from setuptools import setup, find_packages
+from glob import glob
 
 #with open("README.md", "r" ,encoding='utf-8') as fh:
 #    long_description = fh.read()
@@ -7,16 +8,15 @@ from setuptools import setup, find_packages
 setup(
     name="ConfigController",
     version="0.2.0",
-    license='MIT License',
+    license='MIT',
     description="easy control config",
 
     author="Fumiya-Shibamata",
-    author_email="fumi_siba@yahoo.co.jp",
     url="https://github.com/sbfm/ConfigController",
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    #py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     #python_requires='>=3.6',
     #long_description=long_description,
