52ce2cbd42739edd0429ad076242deafc5dfd069
sbfm <fumi_siba@yahoo.co.jp>
2021-11-02 04:53:47 +0900

    pip対応

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+from setuptools import setup
+
+setup()
+
+
 # coding: utf-8
 from setuptools import setup, find_packages
 
@@ -14,8 +19,10 @@ setup(
     author_email="fumi_siba@yahoo.co.jp",
     url="https://github.com/sbfm/ConfigController",
 
-    packages=find_packages(where='src'),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
+    #py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
     #python_requires='>=3.6',
     #long_description=long_description,
     #long_description_content_type="text/markdown",
