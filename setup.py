3df079e03fba6bab1a7b3f0039649c9b10125199
sbfm <fumi_siba@yahoo.co.jp>
2021-11-02 04:18:12 +0900

    pip対応

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={},
+    package_dir={'': 'src'},
     python_requires='>=3.6',
 )
